c49af2f955e35305c456518e5d5b60b53a3d600f
Thu1402 <thule.33254020040@st.ueh.edu.vn>
2026-09-18 23:22:09 +0700

    up

--- Bài 5/bài01/bài01/Program.cs
+++ Bài 5/bài01/bài01/Program.cs
@@ -2,7 +2,7 @@
 {
     class Program
     {
-        static void Main55(string[] args)
+        static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             List<ChuyenXe> list = new List<ChuyenXe>();
